4ea01b290aca228f01afe12fcb74f9d171ab0e56
avanim <axm1205@case.edu>
2022-11-26 09:56:29 -0500

    display first 5 results on site (title, poster, media type, release date, overview)

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,73 @@ fetch(movieRequestURL)
     })
     .then(function (data) {
         console.log(data);
+
+        // Decide on num of results to display (maximum of first 5 results)
+        if (data.results.length <= 5) {
+            var numResults = data.results.length;
+        } else {
+            var numResults = 5;
+        }
+        console.log(numResults);
+
+        // Loop through results and display media info for 5 results (movie or tv)
+        var resultsDisplayed = 1;
+        var i = 0; // index of data.results array
+        while (resultsDisplayed <= numResults) {
+            // Confirm that result's media type is not a person
+            if (data.results[i].media_type !== "person") {
+
+                // Check whether media is movie or TV to get title and release date
+                var title = document.createElement("h2");
+                var releaseDate = document.createElement("li");
+                var genre = document.createElement("li");
+                if (data.results[i].media_type === "movie") {
+                    title.textContent = data.results[i].title;
+                    if (data.results[i].release_date === "") {
+                        releaseDate.textContent = "Release Date: none";
+                    } else {
+                        releaseDate.textContent = "Release Date: " + data.results[i].release_date;
+                    }
+                    // genre = getGenre(data.results[i].genre_ids[0]);
+                    // console.log(genre);
+                } else if (data.results[i].media_type === "tv") {
+                    title.textContent = data.results[i].name;
+                    if (data.results[i].first_air_date === "") {
+                        releaseDate.textContent = "First Air Date: none";
+                    } else {
+                        releaseDate.textContent = "First Air Date: " + data.results[i].first_air_date;
+                    }
+                }
+
+                // Movie/TV title
+                movieSection.appendChild(title);
+
+                // Media poster image
+                if (data.results[i].poster_path !== null) {
+                    var poster = document.createElement("img");
+                    poster.setAttribute("src", "http://image.tmdb.org/t/p/w200//" + data.results[i].poster_path);
+                    movieSection.appendChild(poster);
+                }
+
+                // Media type (movie or tv)
+                var mediaType = document.createElement("li");
+                mediaType.textContent = "Media Type: " + data.results[i].media_type;
+                movieSection.appendChild(mediaType);
+
+                // Release date
+                movieSection.appendChild(releaseDate);
+
+                // Summary/overview
+                var summary = document.createElement("li");
+                if (data.results[i].overview !== "") {
+                    summary.textContent = "Summary: none";    
+                } else {
+                    summary.textContent = "Summary: " + data.results[i].overview;
+                }
+                movieSection.appendChild(summary);
+                
+                resultsDisplayed++;
+            }
+            i++;
+        }
     });
