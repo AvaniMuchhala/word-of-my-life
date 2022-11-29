8d703fa270b824daf86daad31a813c360d71ef05
avanim <axm1205@case.edu>
2022-11-28 21:45:55 -0500

    added movie error if 0 results or only ppl results, added placeholder img for poster

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var synonymMRKey = "d051d5ac-5f7c-4338-8a27-3f68c90156c6";
 var movieAPIKey = "63d7ebc58121dff8f561b458dad5480f";
 
 var wordOfDay = '';
-var movieSection = document.querySelector(".content");
+var mediaSection = document.querySelector("#media");
 
 // URL varriables
 var randomUrl = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=' + wordnikKey;
@@ -13,9 +13,9 @@ var synonymUrl = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json
 
 
 function getMovieData() {
-    movieSection.textContent = "";
+    mediaSection.textContent = "";
 
-    var movieRequestURL = "https://api.themoviedb.org/3/search/multi?query=" + wordOfDay + "&api_key=" + movieAPIKey;
+    var movieRequestURL = "https://api.themoviedb.org/3/search/multi?query=" + "low-end" + "&api_key=" + movieAPIKey;
     fetch(movieRequestURL)
         .then(function (response) {
             return response.json();
@@ -24,75 +24,91 @@ function getMovieData() {
             console.log("Movie/TV results: ");
             console.log(data.results);
 
-            // Decide on num of results to display (maximum of first 5 results)
-            if (data.results.length <= 5) {
-                var numResults = data.results.length;
+            // Movie error in case no elements in "data.results" or all results are people
+            var movieError = document.createElement("h2");
+            movieError.textContent = "No movie or TV content related to this word.";
+            if (data.results.length === 0) {    
+                mediaSection.append(movieError);
             } else {
-                var numResults = 5;
-            }
-
-            // Loop through results and display media info for 5 results (movie or tv)
-            var resultsDisplayed = 1;
-            var i = 0; // index of data.results array
-            while (resultsDisplayed <= numResults) {
-                // If index is out of bounds, break out of while-loop
-                if (i >= data.results.length) {
-                    break;
+                // Decide on num of results to display (maximum of first 5 results)
+                if (data.results.length <= 5) {
+                    var numResults = data.results.length;
+                } else {
+                    var numResults = 5;
                 }
 
-                // Confirm that result's media type is not a person
-                if (data.results[i].media_type !== "person") {
-
-                    // Check whether media is movie or TV to get title and release date
-                    var title = document.createElement("h2");
-                    var releaseDate = document.createElement("li");
-                    // var genre = document.createElement("li");
-                    if (data.results[i].media_type === "movie") {
-                        title.textContent = data.results[i].title;
-                        if (data.results[i].release_date === "") {
-                            releaseDate.textContent = "Release Date: none";
-                        } else {
-                            releaseDate.textContent = "Release Date: " + data.results[i].release_date;
-                        }
-                    } else if (data.results[i].media_type === "tv") {
-                        title.textContent = data.results[i].name;
-                        if (data.results[i].first_air_date === "") {
-                            releaseDate.textContent = "First Air Date: none";
-                        } else {
-                            releaseDate.textContent = "First Air Date: " + data.results[i].first_air_date;
-                        }
+                // Loop through results and display media info for 5 results (movie or tv)
+                var resultsDisplayed = 0;
+                var i = 0; // index of data.results array
+                while (resultsDisplayed < numResults) {
+                    // If index is out of bounds, break out of while-loop
+                    if (i >= data.results.length) {
+                        break;
                     }
 
-                    // Movie/TV title
-                    movieSection.appendChild(title);
+                    // Confirm that result's media type is not a person
+                    if (data.results[i].media_type !== "person") {
 
-                    // Media poster image
-                    if (data.results[i].poster_path !== null) {
+                        // Check whether media is movie or TV to get title and release date
+                        var title = document.createElement("h2");
+                        var releaseDate = document.createElement("p");
+                        // var genre = document.createElement("li");
+                        if (data.results[i].media_type === "movie") {
+                            title.textContent = data.results[i].title;
+                            if (data.results[i].release_date === "") {
+                                releaseDate.textContent = "Release Date: none";
+                            } else {
+                                releaseDate.textContent = "Release Date: " + data.results[i].release_date;
+                            }
+                        } else if (data.results[i].media_type === "tv") {
+                            title.textContent = data.results[i].name;
+                            if (data.results[i].first_air_date === "") {
+                                releaseDate.textContent = "First Air Date: none";
+                            } else {
+                                releaseDate.textContent = "First Air Date: " + data.results[i].first_air_date;
+                            }
+                        }
+
+                        // Movie/TV title
+                        mediaSection.appendChild(title);
+
+                        // Media poster image
                         var poster = document.createElement("img");
-                        poster.setAttribute("src", "http://image.tmdb.org/t/p/w200//" + data.results[i].poster_path);
-                        movieSection.appendChild(poster);
+                        if (data.results[i].poster_path === null) {
+                            poster.setAttribute("src", "https://via.placeholder.com/200x250");
+                            poster.setAttribute("alt", "placeholder image for movie/tv poster");
+                        } else {
+                            poster.setAttribute("src", "https://image.tmdb.org/t/p/w200//" + data.results[i].poster_path);
+                            poster.setAttribute("alt", "movie/tv poster image");
+                        }
+                        mediaSection.appendChild(poster);
+
+                        // Media type (movie or tv)
+                        var mediaType = document.createElement("p");
+                        mediaType.textContent = "Media Type: " + data.results[i].media_type;
+                        mediaSection.appendChild(mediaType);
+
+                        // Release date
+                        mediaSection.appendChild(releaseDate);
+
+                        // Summary/overview
+                        var summary = document.createElement("p");
+                        if (data.results[i].overview === "") {
+                            summary.textContent = "Summary: none";
+                        } else {
+                            summary.textContent = "Summary: " + data.results[i].overview;
+                        }
+                        mediaSection.appendChild(summary);
+
+                        resultsDisplayed++;
                     }
-
-                    // Media type (movie or tv)
-                    var mediaType = document.createElement("li");
-                    mediaType.textContent = "Media Type: " + data.results[i].media_type;
-                    movieSection.appendChild(mediaType);
-
-                    // Release date
-                    movieSection.appendChild(releaseDate);
-
-                    // Summary/overview
-                    var summary = document.createElement("li");
-                    if (data.results[i].overview === "") {
-                        summary.textContent = "Summary: none";
-                    } else {
-                        summary.textContent = "Summary: " + data.results[i].overview;
-                    }
-                    movieSection.appendChild(summary);
-
-                    resultsDisplayed++;
+                    i++;
                 }
-                i++;
+
+                // If no results were displayed (aka all results were people), display movie error
+                if (resultsDisplayed === 0) {
+                    mediaSection.append(movieError);
+                }
             }
         });
 }
@@ -105,14 +121,13 @@ function getDefinition() {
     fetch(defineUrl)
         .then(function (response) {
             console.log(response);
-
-      console.log("Part of Speech: " + data[0].partOfSpeech);
-      console.log(
-        "Definition, " + data[0].attributionText + ": " + data[0].text
-      );
-
-      getDefinitionMR();
-    });
+        })
+        .then(function (data) {
+            console.log("Part of Speech: " + data[0].partOfSpeech);
+            console.log("Definition, " + data[0].attributionText + ": " + data[0].text);
+      
+            getDefinitionMR();
+        });
 }
 
 function getDefinitionMR() {
@@ -121,6 +136,8 @@ function getDefinitionMR() {
     wordOfDay +
     "?key=" +
     defineMRKey;
+    fetch(redefineUrl)
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
@@ -210,116 +227,6 @@ function getWord(event) {
         .then(function (data) {
             console.log(data);
 
-            alert('The word of the day is: ' + wordOfDay + '.');
-
-            // Display word of day in textbox
-            var wordBox = document.querySelector(".input");
-            wordBox.value = wordOfDay;
-
-            if (confirm('Would you like a new word?')) {
-                getWord();
-            } else {
-                alert('Cool. Enjoy your day!');
-
-                getDefinition();
-                // getSynonyms();
-
-getWord();
-
-wordEl = document.getElementById("word");
-wordEl.textContent = wordOfDay;
-var movieSection = document.querySelector("#movie-data");
-var movieRequestURL =
-  "https://api.themoviedb.org/3/search/multi?query=" +
-  wordOfDay +
-  "&api_key=" +
-  movieAPIKey;
-
-fetch(movieRequestURL)
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.results);
-
-    // Decide on num of results to display (maximum of first 5 results)
-    if (data.results.length <= 5) {
-      var numResults = data.results.length;
-    } else {
-      var numResults = 5;
-    }
-
-    // Loop through results and display media info for 5 results (movie or tv)
-    var resultsDisplayed = 1;
-    var i = 0; // index of data.results array
-    while (resultsDisplayed <= numResults) {
-      // If index is out of bounds, break out of while-loop
-      if (i >= data.results.length) {
-        break;
-      }
-
-      // Confirm that result's media type is not a person
-      if (data.results[i].media_type !== "person") {
-        // Check whether media is movie or TV to get title and release date
-        var title = document.createElement("h2");
-        var releaseDate = document.createElement("li");
-        var genre = document.createElement("li");
-        if (data.results[i].media_type === "movie") {
-          title.textContent = data.results[i].title;
-          if (data.results[i].release_date === "") {
-            releaseDate.textContent = "Release Date: none";
-          } else {
-            releaseDate.textContent =
-              "Release Date: " + data.results[i].release_date;
-          }
-        } else if (data.results[i].media_type === "tv") {
-          title.textContent = data.results[i].name;
-          if (data.results[i].first_air_date === "") {
-            releaseDate.textContent = "First Air Date: none";
-          } else {
-            releaseDate.textContent =
-              "First Air Date: " + data.results[i].first_air_date;
-          }
-        }
-
-        // Movie/TV title
-        movieSection.appendChild(title);
-
-        // Media poster image
-        if (data.results[i].poster_path !== null) {
-          var poster = document.createElement("img");
-          poster.setAttribute(
-            "src",
-            "http://image.tmdb.org/t/p/w200//" + data.results[i].poster_path
-          );
-          movieSection.appendChild(poster);
-        }
-
-        // Media type (movie or tv)
-        var mediaType = document.createElement("li");
-        mediaType.textContent = "Media Type: " + data.results[i].media_type;
-        movieSection.appendChild(mediaType);
-
-        // Release date
-        movieSection.appendChild(releaseDate);
-
-        // Summary/overview
-        var summary = document.createElement("li");
-        if (data.results[i].overview === "") {
-          summary.textContent = "Summary: none";
-        } else {
-          summary.textContent = "Summary: " + data.results[i].overview;
-        }
-        movieSection.appendChild(summary);
-
-        resultsDisplayed++;
-      }
-      i++;
-    }
-  });
-
-  
             wordOfDay = data.word;
             console.log(wordOfDay);
 
@@ -334,7 +241,7 @@ fetch(movieRequestURL)
             } else {
                 alert('Cool. Enjoy your day!');
 
-                getDefinition();
+                // getDefinition();
                 // getSynonyms();
 
                 getMovieData();
@@ -342,5 +249,6 @@ fetch(movieRequestURL)
         })
 }
 
+//getWord();
 var button = document.querySelector(".button");
 button.addEventListener("click", getWord);
